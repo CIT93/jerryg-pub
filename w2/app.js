72ff51cb691a43b890714744de93a123adca3610
Jerry Guerrero <gguerrero22@my.scccd.edu>
2026-08-20 21:57:50 -0700

    Step 2.3 Complete

--- w2/app.js
+++ w2/app.js
@@ -16,26 +16,26 @@ const itemPrice = 15;
 const handleButtonClick = function() {
     // totalCost = totalCost +1;
     // Increase totalCost by 1 each time the button is clicked
-    totalCost += 1;
+    totalCost += itemPrice;
     
     // Template strings (literal) to easily combine our variables and text into one message
-    let message = `Hello! You have clicked the button ${totalCost} time(s).`;
+    let message = `Current Total: $${totalCost}`;
 
     // This is basic decision-making in JavaScript!
     // Use a simple 'if' statement to make our page react differently based on totalCost.
-    if(totalCost >= 5) {
+    if(totalCost >= 60) {
         // We can even change the style of an HTML element directly with JavaScript!
         // Change text color
-        message += ' WOW, you are a super clicker!';
-        totalDisplayElement.style.color = 'purple';
+        message += ' (Over Budget!)';
+        totalDisplayElement.style.color = 'red';
     } else {
-        totalDisplayElement.style.color = '#333';
+        totalDisplayElement.style.color = 'black';
     }
     // Update the text content of our paragraph element on the page.
     // This is how JavaScript makes changes visible on the web page!
     totalDisplayElement.textContent = message;
        
-    console.log(`Button Clicked! Current click count: ${totalCost}`);
+    console.log(`Button Clicked! Current total: $${totalCost}`);
 };
 
 
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function(){
     // When 'addButton' receives a 'click' event, the 'handleButtonClick' function will execute.
     addButton.addEventListener('click', handleButtonClick);
 
-    totalDisplayElement.textContent = `Welcome! Click the button below to start counting`;
+    totalDisplayElement.textContent = `Current Total: $${totalCost}`;
 })
 
 
